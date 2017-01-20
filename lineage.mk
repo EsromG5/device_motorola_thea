ce49a8246792b5b16603b99577b62c05453f3802
EsromG5 <esromgalaxy5@gmail.com>
2017-01-20 15:24:31 -0300

    fix for lineage dependencies

--- lineage.mk
+++ lineage.mk
@@ -1,10 +1,10 @@
 $(call inherit-product, device/motorola/thea/full_thea.mk)
 
 # Inherit some common AOKP stuff.
-$(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
+$(call inherit-product, vendor/cm/configs/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := thea
-PRODUCT_NAME := aokp_thea
+PRODUCT_NAME := lineage_thea
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
